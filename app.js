fb48d7ebbcb3dcae6bab2d36fa70f77b675f01d6
Caitlin <lomcaitlin@gmail.com>
2020-04-05 23:56:30 -0700

    helmet package

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var express = require("express");
+var helmet = require("helmet");
 var app = express();
 
 app.use(express.static("public"));
+app.use(helmet());
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
-    res.render("index");
+    res.render("index", {active: "index"});
 });
 
 app.get("/html", function(req, res) {
-    res.render("html");
+    res.render("html", {active: "html"});
 });
 
 app.get("/css", function(req, res) {
-    res.render("css");
+    res.render("css", {active: "css"});
 });
 
 app.get("/js", function(req, res) {
-    res.render("js");
+    res.render("js", {active: "js"});
 });
 
 app.get("*", function(req, res) {
